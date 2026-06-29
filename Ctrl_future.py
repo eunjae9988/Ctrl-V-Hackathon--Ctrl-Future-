061d8be65d093b26fb705928d1727dfc5b67db83
eunjae9988 <eunjae9988@gmail.com>
2026-06-29 11:42:06 -0700

    Add scholarship data structure

---
+++ Ctrl_future.py
@@ -0,0 +1,13 @@
+scholarships = [
+    {"name": "STEM Excellence Scholarship",
+        "gpa": 3.5,
+        "major": "STEM",
+        "amount": "$5,000"},
+    {"name": "First Generation Student Grant",
+        "gpa": 2.8,
+        "major": "Any",
+        "amount": "$3,000"},
+    {"name": "Women in Technology Scholarship",
+        "gpa": 3.0,
+        "major": "Computer Science",
+        "amount": "$10,000"}]
